c1e13016b8156f33b72d014aea4e0a74ee9bb56a
softApp-eng <softmed2018@gmail.com>
2022-12-27 14:57:13 +0100

    changes

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/@fuse/demo'));
-app.get('/*', function(req,res)
-
-{res.sendFile(path.join(__dirname+'/dist/@fuse/demo/index.html'));});
-
-app.listen(process.env.PORT || 8080);
